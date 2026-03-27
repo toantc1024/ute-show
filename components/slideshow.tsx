6efb0d1d3929393ee3597aa8097369f6c51868dc
Vo Minh Hoa <thanhnoobs@gmail.com>
2026-03-27 23:10:30 +0700

    upload medium pic

--- components/slideshow.tsx
+++ components/slideshow.tsx
@@ -10,15 +10,15 @@ import {
 import Autoplay from "embla-carousel-autoplay"
 
 const IMAGES = [
-  "16-17-01.png", "16-17-02.png", "16-17-03.png", "16-17-04.png",
-  "17-18-01.png", "17-18-02.png", "17-18-03.png", "17-18-04.png",
-  "18-19-01.png", "18-19-02.png", "18-19-03.png", "18-19-04.png",
-  "19-21-01.png", "19-21-02.png", "19-21-03.png", "19-21-04.png",
-  "21-22-01.png", "21-22-02.png", "21-22-03.png", "21-22-04.png",
-  "22-23-01.png", "22-23-02.png", "22-23-03.png", "22-23-04.png",
-  "23-24-01.png", "23-24-02.png", "23-24-03.png", "23-24-04.png",
-  "24-25-01.png", "24-25-02.png", "24-25-03.png", "24-25-04.png",
-  "25-26-01.png", "25-26-02.png", "25-26-03.png", "25-26-04.png"
+  "16-17-01 Medium.png", "16-17-02 Medium.png", "16-17-03 Medium.png", "16-17-04 Medium.png",
+  "17-18-01 Medium.png", "17-18-02 Medium.png", "17-18-03 Medium.png", "17-18-04 Medium.png",
+  "18-19-01 Medium.png", "18-19-02 Medium.png", "18-19-03 Medium.png", "18-19-04 Medium.png",
+  "19-21-01 Medium.png", "19-21-02 Medium.png", "19-21-03 Medium.png", "19-21-04 Medium.png",
+  "21-22-01 Medium.png", "21-22-02 Medium.png", "21-22-03 Medium.png", "21-22-04 Medium.png",
+  "22-23-01 Medium.png", "22-23-02 Medium.png", "22-23-03 Medium.png", "22-23-04 Medium.png",
+  "23-24-01 Medium.png", "23-24-02 Medium.png", "23-24-03 Medium.png", "23-24-04 Medium.png",
+  "24-25-01 Medium.png", "24-25-02 Medium.png", "24-25-03 Medium.png", "24-25-04 Medium.png",
+  "25-26-01 Medium.png", "25-26-02 Medium.png", "25-26-03 Medium.png", "25-26-04 Medium.png"
 ].map(file => `/assets/BGD/${file}`)
 
 export function Slideshow({ className }: { className?: string }) {
